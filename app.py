b1eba43478c2e995ae7c9785a16b662e151403fa
wassimtajeddin <wassim.tajeddin@gmail.com>
2025-02-15 11:58:24 +0100

    Add delete function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import pymongo
+from tkinter import messagebox
 
 # Connect to mongoDB
 client = pymongo.MongoClient("mongodb://localhost:27017")
@@ -24,6 +25,7 @@ def send_message():
 send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack(pady=5)
 
+
 messages_label = tk.Label(root, text="Messages:\n") 
 messages_label.pack()
 
@@ -31,6 +33,22 @@ def fetch_messages():
     messages = message_col.find().sort("_id", -1)
     messages_label.config(text="Messages:\n" + "\n".join(f"-{m['text']}"for m in messages))
     root.after(2000, fetch_messages)
+
+
+
+def confirm_delete_messages():
+    response = messagebox.askyesno("Confirm Deletion", "Are you sure you want to delete all messages?")
+    if response:
+        delete_all_messages()
+        
+        
+delete_button = tk.Button(root, text="Delete Messages", command=confirm_delete_messages)
+delete_button.pack(pady=5)
+    
+        
+def delete_all_messages():
+    message_col.delete_many({})
+    fetch_messages()
 
 fetch_messages()
 root.mainloop()
